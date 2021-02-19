d77cae71ac6e5f4312dd7e2518d70dee8482f662
Clarke Lamothe <clarkelamothe@yahoo.fr>
2021-02-19 03:19:22 -0300

    update app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = 'Piano App';
   playSound(note: string): void {
     const audio = new Audio();
-    audio.src = '../assets/notes/' + note + '.wav';
+    audio.src = '../assets/notes/' + note + '.mp3';
     audio.load();
     audio.play();
     console.log(audio.src);
